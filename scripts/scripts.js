2061dda2775d5bccc939fdb9586b0cb7cebd00b7
SuNgu <cliffland.jasungu@gmail.com>
2024-03-04 17:43:22 +0300

    managed to remove error preventing signin button from clicking

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -4,38 +4,38 @@ document.getElementById('sign-in-form').addEventListener('submit', (event) => {
     
 });
 
-let signinform = document.getElementById('sign-in-submit').addEventListener('click', SignIn);
+// let signinform = document.getElementById('sign-in-submit').addEventListener('click', SignIn);
 
-function SignUp(e) {
-    e.preventDefault();
-    const email = signupemail.value;
-    const password = signuppassword.value;
+// function SignIn(e) {
+//     e.preventDefault();
+//     const email = signinemail.value;
+//     const password = signinpassword.value;
   
-    // Create the 'credentials' object
-    const credentials = {
-      email: email,
-      password: password
-    };
+//     // Create the 'credentials' object
+//     const credentials = {
+//       email: email,
+//       password: password
+//     };
   
-    // Validate the email and password
-    if (validateEmail(email) && validatePassword(password)) {
-      firebase
-        .auth()
-        .createUserWithEmailAndPassword(credentials.email, credentials.password)
-        .then(function(userCredential) {
-          // Signed in
-          var user = userCredential.user;
-          console.log("User created successfully", user);
-          signupform.reset();
-          window.location.href = "login.html";
-        })
-        .catch(function(error) {
-          // Handle Errors here.
-          var errorCode = error.code;
-          var errorMessage = error.message;
-          console.log("Error creating user", errorCode, errorMessage);
-        });
-    } else {
-      alert("Please enter a valid email and password.");
-    }
-  }
+//     // Validate the email and password
+//     if (validateEmail(email) && validatePassword(password)) {
+//       firebase
+//         .auth()
+//         .signInWithEmailAndPassword(credentials.email, credentials.password)
+//         .then(function(userCredential) {
+//           // Signed in
+//           var user = userCredential.user;
+//           console.log("User created successfully", user);
+//           signupform.reset();
+//           window.location.href = "index.html";
+//         })
+//         .catch(function(error) {
+//           // Handle Errors here.
+//           var errorCode = error.code;
+//           var errorMessage = error.message;
+//           console.log("Error creating user", errorCode, errorMessage);
+//         });
+//     } else {
+//       alert("Please enter a valid email and password.");
+//     }
+//   }
